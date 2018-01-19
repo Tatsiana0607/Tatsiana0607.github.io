35c5bab4830c5c8e6ff877a7a44798a2bb28c77d
Tatsiana0607 <tatsiana_0607@tut.by>
2018-01-19 10:03:00 +0300

    create home page layout

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -5,7 +5,8 @@ var navbarOnScroll = function() {
         $('.logo-img-active').css('display', 'inline-block');
         $('.logo-img-main').css('display', 'none');
         $('.navbar').css('background', 'white');
-        $('.navbar').css('border-bottom', '1px solid rgba(180, 180, 180, 0.2)');
+        $('.navbar').css('border-bottom', 'none');
+        $('.navbar').css('box-shadow', '0px 0px 8px lightgrey');
         $('.nav li a').removeClass('color-white');
         $('.nav li a').addClass('color-heading');
     }
@@ -14,17 +15,18 @@ var navbarOnScroll = function() {
         $('.logo-img-main').css('display', 'inline-block');
         $('.navbar').css('background', 'transparent');
         $('.navbar').css('border-bottom', '1px solid rgba(255, 255, 255, 0.2)');
+        $('.navbar').css('box-shadow', 'none');
         $('.nav li a').removeClass('color-heading');
         $('.nav li a').addClass('color-white');
     }
 }
 
 $(document).ready(function() {
+
+    navbarOnScroll();
+
     $(window).scroll(function() {
         navbarOnScroll();
     });
 });
-
-$(document).onload
-
 
