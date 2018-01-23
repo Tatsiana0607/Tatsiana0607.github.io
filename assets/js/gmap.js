bd022db54c2e512703bd2a666dcd81eb1e53d507
Tatsiana0607 <tatsiana_0607@tut.by>
2018-01-23 22:52:15 +0300

    create contact page

---
+++ assets/js/gmap.js
@@ -0,0 +1,69 @@
+"use strict";
+
+window.initMap = function() {
+  var customMapType = new google.maps.StyledMapType([
+    {
+      stylers: [
+        {'saturation': -100},
+        {'lightness': 50},
+        {'visibility': 'simplified'}
+      ]
+    },
+    {
+      elementType: 'labels',
+      stylers: [{visibility: 'on'}]
+    }
+  ], {
+    name: 'Dublin'
+  });
+
+  var image = new google.maps.MarkerImage(
+  	'img/widgets/gmap-pin.png',
+  	new google.maps.Size(48,54),
+  	new google.maps.Point(0,0),
+  	new google.maps.Point(24,54)
+	);
+
+  var customMapTypeId = 'custom_style';
+
+  var brooklyn = {lat:53.9034,lng:-332.4422};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    scrollwheel: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    center: brooklyn, // Brooklyn.
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+    }
+  });
+
+  var contentString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h4 id="firstHeading" class="firstHeading">Офис в Минске</h4>'+
+    '<div id="bodyContent">'+
+    '<p>Площадь Свободы 23, офис 35А<br>Минск 220030<br>Беларусь</p>'+
+    '</div>'+
+    '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 300
+  });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    clickable: true,
+    icon: image,
+    title: 'Brooklyn',
+    position: brooklyn
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId);
+}
