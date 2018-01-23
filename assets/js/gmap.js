c76d9a94d930c50f6d3777bd77ede270ba07480b
Tatsiana0607 <tatsiana_0607@tut.by>
2018-01-23 23:34:59 +0300

    edit map on the contact page

--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -1,69 +1,48 @@
-"use strict";
 
 window.initMap = function() {
-  var customMapType = new google.maps.StyledMapType([
+    var customMapType = new google.maps.StyledMapType([
     {
-      stylers: [
-        {'saturation': -100},
-        {'lightness': 50},
-        {'visibility': 'simplified'}
-      ]
-    },
-    {
-      elementType: 'labels',
-      stylers: [{visibility: 'on'}]
-    }
-  ], {
-    name: 'Dublin'
-  });
+        stylers: [
+            {'saturation': -100},
+            {'lightness': 20},
+        ]
+    }]);
 
-  var image = new google.maps.MarkerImage(
-  	'img/widgets/gmap-pin.png',
+    var customMapTypeId = 'custom_style';
+
+    var image = new google.maps.MarkerImage(
+  	'assets/img/gmap-pin.png',
   	new google.maps.Size(48,54),
-  	new google.maps.Point(0,0),
-  	new google.maps.Point(24,54)
+  	new google.maps.Point(0,0)
 	);
 
-  var customMapTypeId = 'custom_style';
+    var minsk = {lat: 53.9034, lng: -332.4422};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: minsk,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+        }
+    });
 
-  var brooklyn = {lat:53.9034,lng:-332.4422};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    scrollwheel: false,
-    streetViewControl: false,
-    mapTypeControl: false,
-    center: brooklyn, // Brooklyn.
-    mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
-    }
-  });
+    var contentString = '<h4 id="firstHeading" class="firstHeading">Офис в Минске</h4>' +
+                        '<p>Площадь Свободы 23, офис 35А<br>Минск 220030<br>Беларусь</p>';
 
-  var contentString = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h4 id="firstHeading" class="firstHeading">Офис в Минске</h4>'+
-    '<div id="bodyContent">'+
-    '<p>Площадь Свободы 23, офис 35А<br>Минск 220030<br>Беларусь</p>'+
-    '</div>'+
-    '</div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
 
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 300
-  });
+    var marker = new google.maps.Marker({
+        map: map,
+        clickable: true,
+        icon: image,
+        position: minsk
+    });
 
-  var marker = new google.maps.Marker({
-    map: map,
-    clickable: true,
-    icon: image,
-    title: 'Brooklyn',
-    position: brooklyn
-  });
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
 
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-
-  map.mapTypes.set(customMapTypeId, customMapType);
-  map.setMapTypeId(customMapTypeId);
+    map.mapTypes.set(customMapTypeId, customMapType);
+    map.setMapTypeId(customMapTypeId);
 }
