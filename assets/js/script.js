54e23d24144941e19de1ee125e9a6d3d0727a235
Tatsiana0607 <tatsiana_0607@tut.by>
2018-01-24 08:21:30 +0300

    edit script

---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+﻿
+// прелоадер
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
+
+
+// изменение меню при прокрутке
+var navbarOnScroll = function() {
+    if ($(window).scrollTop() > 50) {
+        $('.logo-img-active').css('display', 'inline-block');
+        $('.logo-img-main').css('display', 'none');
+        $('.navbar').css('background', 'white');
+        $('.navbar').css('border-bottom', 'none');
+        $('.navbar').css('box-shadow', '0px 0px 8px lightgrey');
+        $('.nav li a').removeClass('color-white');
+        $('.nav li a').addClass('color-heading');
+    }
+    else {
+        $('.logo-img-active').css('display', 'none');
+        $('.logo-img-main').css('display', 'inline-block');
+        $('.navbar').css('background', 'transparent');
+        $('.navbar').css('border-bottom', '1px solid rgba(255, 255, 255, 0.2)');
+        $('.navbar').css('box-shadow', 'none');
+        $('.nav li a').removeClass('color-heading');
+        $('.nav li a').addClass('color-white');
+    }
+}
+
+$(document).ready(function() {
+
+    navbarOnScroll();
+
+    $(window).scroll(function() {
+        navbarOnScroll();
+    });
+});
+
+
+//подключение Google Maps
+
+window.initMap = function() {
+    var customMapType = new google.maps.StyledMapType([
+    {
+        stylers: [
+            {'saturation': -100},
+            {'lightness': 20},
+        ]
+    }]);
+
+    var customMapTypeId = 'custom_style';
+
+    var image = new google.maps.MarkerImage(
+  	'assets/img/gmap-pin.png',
+  	new google.maps.Size(48,54),
+  	new google.maps.Point(0,0)
+	);
+
+    var minsk = {lat: 53.9034, lng: -332.4422};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: minsk,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+        }
+    });
+
+    var contentString = '<h4 id="firstHeading" class="firstHeading">Офис в Минске</h4>' +
+                        '<p>Площадь Свободы 23, офис 35А<br>Минск 220030<br>Беларусь</p>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        map: map,
+        clickable: true,
+        icon: image,
+        position: minsk
+    });
+
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+
+    map.mapTypes.set(customMapTypeId, customMapType);
+    map.setMapTypeId(customMapTypeId);
+}
+
