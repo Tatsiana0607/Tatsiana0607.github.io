2fd79f1f9a59257c5d5ff776e21a4956efb1f3d8
Tatsiana0607 <tatsiana_0607@tut.by>
2018-03-07 18:55:51 +0300

    create function uploading photos from the storage

--- assets/js/firebase-manager.js
+++ assets/js/firebase-manager.js
@@ -1,4 +1,16 @@
-const URL_TOURS ="https://polar-star.firebaseio.com/tours.json";
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyD6ATxEe58Kw1MypGzHn2wTqPtHJjLe7Dg",
+    authDomain: "polar-star.firebaseapp.com",
+    databaseURL: "https://polar-star.firebaseio.com",
+    projectId: "polar-star",
+    storageBucket: "polar-star.appspot.com",
+    messagingSenderId: "185259417758"
+};
+firebase.initializeApp(config);
+
+const URL_TOURS_DATA ="https://polar-star.firebaseio.com/tours.json";
+const URL_TOURS_IMAGES = firebase.storage().ref('tours');
 
 let vueApp = new Vue({
     el: '#catalog',
@@ -6,17 +18,27 @@ let vueApp = new Vue({
         tours: []
     },
     methods: {
-        getTours: function () {
+        getToursData: function () {
             $.ajax({
-                url:URL_TOURS, type:'GET', dataType:'json'
+                url:URL_TOURS_DATA, type:'GET', dataType:'json'
             }).then((resp) => {
                 this.tours = resp;
             }).catch((err) => {
                 console.log('Error', err.message);
             })
+        },
+        getImageURL: function (image) {
+            return URL_TOURS_IMAGES.child(image+'.jpg').getDownloadURL()
+                .then((url) => {
+                    console.log(url);
+                    return url;
+                    // document.getElementById('_'+image).src = url;
+            }).catch((err) => {
+                console.log('Error', err.message);
+            });
         }
     },
     created: function () {
-        this.getTours();
+        this.getToursData();
     }
 });
