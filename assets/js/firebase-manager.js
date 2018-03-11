5071c449562efc2e8c6cf07897f42b336e8e77e8
Tatsiana0607 <tatsiana_0607@tut.by>
2018-03-11 15:23:26 +0300

    add filtration

--- assets/js/firebase-manager.js
+++ assets/js/firebase-manager.js
@@ -14,17 +14,27 @@ let URL_TOURS_DATA ="https://polar-star.firebaseio.com/tours.json";
 let vueApp = new Vue({
     el: '#catalog',
     data: {
-        search: '',
         countries: [
-            {id: 'norway', value: 'Норвегия', status: false},
-            {id: 'finland', value: 'Финляндия', status: false},
-            {id: 'russia', value: 'Росиия', status: false},
-            {id: 'usa', value: 'США', status: false},
-            {id: 'canada', value: 'Канада', status: false},
-            {id: 'iceland', value: 'Исландия', status: false},
-            {id: 'sweden', value: 'Швеция', status: false},
-            {id: 'denmark', value: 'Дания', status: false}],
-        tours: []
+            {id: 'norway', value: 'Норвегия'},
+            {id: 'finland', value: 'Финляндия'},
+            {id: 'russia', value: 'Россия'},
+            {id: 'usa', value: 'США'},
+            {id: 'canada', value: 'Канада'},
+            {id: 'iceland', value: 'Исландия'},
+            {id: 'sweden', value: 'Швеция'},
+            {id: 'denmark', value: 'Дания'}],
+        options:[
+            {value: '', text: 'Сортировка'},
+            {value: 'price', text: 'по цене'},
+            {value: 'days', text: 'по длительности'}
+        ],
+        tours: [],
+        search: '',
+        dateValues: {start:'', end:''},
+        priceValues: {min: 0, max: 0},
+        durationValues:{min: 0, max: 0},
+        sorting: '',
+        checkedCountries: [],
     },
     methods: {
         getToursData: function () {
@@ -36,13 +46,91 @@ let vueApp = new Vue({
                 console.log('Error', err.message);
             })
         },
-        changeCheckboxURL: function (event) {
-            if (event) {
-                console.log(event.target.id);
+    },
+    computed:{
+        filteredTours: function(){
+            let countries = this.checkedCountries;
+            let price = this.priceValues;
+            let days = this.durationValues;
+            let date = this.dateValues;
+            let criteria = this.sorting;
+
+            let filtered = this.tours.filter(function (elem) {
+                if(countries.length){
+                    return countries.some(item => {return item===elem.country;});
+                }
+                return true;
+            }).filter(function (elem) {
+                return elem.price >= price.min && elem.price <= price.max;
+            }).filter(function (elem) {
+                return elem.days >= days.min && elem.days <= days.max;
+            }).filter(function (elem) {
+                if(!date.start && !date.end) return true;
+                else if(date.start && !date.end) return moment(elem.date) >= moment(date.start);
+                else if(!date.start && date.end) return moment(elem.date) <= moment(date.end);
+                return moment(elem.date) >= moment(date.start) && moment(elem.date) <= moment(date.end);
+            });
+
+            if(criteria){
+                filtered.sort(function (a, b) {
+                    if (a[criteria] > b[criteria]) {return 1;}
+                    if (a[criteria] < b[criteria]) {return -1;}
+                    return 0;
+                });
             }
+            return filtered;
+        },
+    },
+    filters: {
+        formatDate(date){
+            let options = {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+            };
+            moment.locale('ru');
+            return moment(date).format('LL');
         }
     },
     created: function () {
         this.getToursData();
+    },
+    mounted: function () {
+        let vm = this;
+        $('#date-start').datepicker({
+            onSelect: function(date) {
+                vm.dateValues.start = date;
+            }
+        });
+        $('#date-end').datepicker({
+            onSelect: function(date) {
+                vm.dateValues.end = date;
+            }
+        })
     }
 });
+
+
+//компоненты jQuery UI
+$('#price-slider').slider( { min: 100, max: 2000, step:10, animate: 500, range: true, values:[100,2000],slide: printValues, change: printValues } );
+$('#duration-slider').slider( { min: 1, max: 30, step:1, animate: 500, range: true, values:[1,30],slide: printValues, change: printValues } );
+
+function printValues() {
+    let minPrice = $('#price-slider').slider('values',0);
+    let maxPrice = $('#price-slider').slider('values',1);
+    vueApp.priceValues.min = minPrice;
+    vueApp.priceValues.max = maxPrice;
+    let maxDuration = $('#duration-slider').slider('values',0);
+    let minDuration = $('#duration-slider').slider('values',1);
+    vueApp.durationValues.min = maxDuration;
+    vueApp.durationValues.max = minDuration;
+}
+printValues();
+
+$('#country').controlgroup();
+$('#pagination').controlgroup();
+$('#sort').selectmenu({change:changed});
+
+function changed(event,ui) {
+    vueApp.sorting = ui.item.value;
+}
