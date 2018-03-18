fcb9e154ed0a6c980c8067445c7c76444528fb2f
Tatsiana0607 <tatsiana_0607@tut.by>
2018-03-19 01:10:23 +0300

    add sending tour request

--- assets/js/tour-vue.js
+++ assets/js/tour-vue.js
@@ -22,6 +22,40 @@ let vueTour = new Vue({
             }).catch((err) => {
                 console.log('Error', err.message);
             });
+        },
+        addTourRequest: function () {
+            let client = vueUser.currentUser;
+            let uid = client.email.replace('.', '');
+
+            if(client.requests){
+                if(client.requests[this.tour.uid]){
+                    $("#modalRequestDenied").modal("show");
+                }
+                else sendRequest();
+            }
+            else sendRequest();
+
+            function sendRequest() {
+                $("#modalRequestAccepted").modal("show");
+                let request = {
+                    [vueTour.tour.uid]: {
+                        name: vueTour.tour.name,
+                        status: 'processing'
+                    }
+                };
+                $.ajax({
+                    url: URL_USERS_DATA + "/" + uid + "/requests.json",
+                    type: 'PATCH',
+                    data: JSON.stringify(request),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: 'json'
+                }).then((result) => {
+                    console.log('Заявка подана', result);
+                    vueUser.getUserData(uid);
+                }).catch((err) => {
+                    console.log('Error', err.message);
+                });
+            }
         }
     },
     created: function () {
